948fb7267e373872c290ee41be9022738d537e3f
베르니체 <vertniche@beleuniche-ui-MacBookPro.local>
2026-09-04 17:51:20 +0900

    site: pages.dev → vertniche.com 리디렉션을 Pages Function으로

---
+++ functions/_middleware.js
@@ -0,0 +1,5 @@
+export const onRequest = ({ request, next }) => {
+  const url = new URL(request.url);
+  if (url.hostname.endsWith('.pages.dev')) { url.hostname = 'vertniche.com'; return Response.redirect(url.toString(), 301); }
+  return next();
+};
